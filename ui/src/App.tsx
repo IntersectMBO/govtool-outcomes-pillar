1621cbfe4e338eebb1ea6e5bcfc4b281d69c53ed
Cindy <cindyjoan2000@gmail.com>
2025-01-22 21:07:34 +0300

    feat: governance action cards

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,3 +1,6 @@
+import { Box, Grid } from "@mui/material";
+import GovernanceActionCard from "./Components/GovernanceActionCard";
+import { IconArrowCircleRight } from "@intersect.mbo/intersectmbo.org-icons-set";
 import "./index.scss";
 
 export type AppProps = {
@@ -12,8 +15,58 @@ function App({ description }: AppProps) {
         height: "100%",
       }}
     >
-      <h1>govtool-outcomes-pillar-ui hel</h1>
-      <p>{description}</p>
+      <Box 
+      sx={{
+        padding: 2,
+        maxWidth: '1200px',
+        margin: '0 auto', // Center the content
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={4}>
+          <GovernanceActionCard
+            dateSubmitted="18 Jan 2024"
+            epoch={430}
+            status="Ratified"
+            title="Plutus V3 Cost Model Parameter Changes Prior to Chang#2"
+            abstract="In light of recent network congestion and scalability concerns I propose implementing a dynamic fee adjustment mechanism that autonomously regulates transaction fees based on network demand and resource availability."
+            governanceActionType="Protocol Parameter Changes"
+            governanceActionID="cnewjfbe82rg39udbjwbksomething"
+            cipGovernanceActionID="cnewjfbe82rg39udbjwbksomething"
+            statusDate="22nd Jan 2024"
+            statusEpoch={440}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <GovernanceActionCard
+            dateSubmitted="18 Jan 2024"
+            epoch={430}
+            status="Expired"
+            title="Plutus V3 Cost Model Parameter Changes Prior to Chang#2"
+            abstract="In light of recent network congestion and scalability concerns I propose implementing a dynamic fee adjustment mechanism that autonomously regulates transaction fees based on network demand and resource availability."
+            governanceActionType="Protocol Parameter Changes"
+            governanceActionID="cnewjfbe82rg39udbjwbksomething"
+            cipGovernanceActionID="cnewjfbe82rg39udbjwbksomething"
+            statusDate="22nd Jan 2024"
+            statusEpoch={440}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <GovernanceActionCard
+            dateSubmitted="18 Jan 2024"
+            epoch={430}
+            status="Enacted"
+            title="Plutus V3 Cost Model Parameter Changes Prior to Chang#2"
+            abstract="In light of recent network congestion and scalability concerns I propose implementing a dynamic fee adjustment mechanism that autonomously regulates transaction fees based on network demand and resource availability."
+            governanceActionType="Protocol Parameter Changes"
+            governanceActionID="cnewjfbe82rg39udbjwbksomething"
+            cipGovernanceActionID="cnewjfbe82rg39udbjwbksomething"
+            statusDate="22nd Jan 2024"
+            statusEpoch={440}
+          />
+        </Grid>
+      </Grid>
+    </Box>
     </div>
   );
 }
