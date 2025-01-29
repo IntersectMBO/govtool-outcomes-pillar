8707a03bd53ae006d98c92319dc114c0b94d4169
Cindy <cindyjoan2000@gmail.com>
2025-01-29 09:55:53 +0300

    LN-136feature/-gov-actions-list-component-frontend

--- ui/src/Pages/Outcomes.tsx
+++ ui/src/Pages/Outcomes.tsx
@@ -5,40 +5,84 @@ import { GOVERNANCE_ACTION_FILTERS } from "../consts/filters";
 import { GOVERNANCE_ACTION_STATUS_FILTERS } from "../consts/status-filters";
 import { GOVERNANCE_ACTION_SORT_OPTIONS } from "../consts/sort-options";
 import { useState } from "react";
-
+import GovernanceActionsList from "../Components/Molecules/GovernanceAvtionsList";
 
 export default function OutcomesPage() {
   const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
   const [searchText, setSearchText] = useState("");
-  const [filtersOpen, setFilterOpen] = useState(false)
-  const [sortsOpen, setSortsOpen] = useState(false)
-  const [selectedSorting, setSelectedSorting] = useState("")
-  const closeFilters = () =>{
-    setFilterOpen(false)
-  }
-  const closeSorts = () =>{
-    setSortsOpen(false)
-  }
+  const [filtersOpen, setFilterOpen] = useState(false);
+  const [sortsOpen, setSortsOpen] = useState(false);
+  const [selectedSorting, setSelectedSorting] = useState("");
+  const closeFilters = () => {
+    setFilterOpen(false);
+  };
+  const closeSorts = () => {
+    setSortsOpen(false);
+  };
+  const governanceActions = [
+    {
+      dateSubmitted: "18 Jan 2024",
+      epoch: 430,
+      status: "Ratified",
+      title: "Plutus V3 Cost Model Parameter Changes Prior to Chang#2",
+      abstract:
+        "In light of recent network congestion and scalability concerns I propose implementing a dynamic fee adjustment mechanism that autonomously regulates transaction fees based on network demand and resource availability.",
+      governanceActionType: "Protocol Parameter Changes",
+      governanceActionID: "cnewjfbe82rg39udbjwbksomething",
+      cipGovernanceActionID: "cnewjfbe82rg39udbjwbksomething",
+      statusDate: "22nd Jan 2024",
+      statusEpoch: 440,
+    },
+    {
+      dateSubmitted: "18 Jan 2024",
+      epoch: 430,
+      status: "Expired",
+      title: "Plutus V3 Cost Model Parameter Changes Prior to Chang#2",
+      abstract:
+        "In light of recent network congestion and scalability concerns I propose implementing a dynamic fee adjustment mechanism that autonomously regulates transaction fees based on network demand and resource availability.",
+      governanceActionType: "Protocol Parameter Changes",
+      governanceActionID: "cnewjfbe82rg39udbjwbksomething",
+      cipGovernanceActionID: "cnewjfbe82rg39udbjwbksomething",
+      statusDate: "22nd Jan 2024",
+      statusEpoch: 440,
+    },
+    {
+      dateSubmitted: "18 Jan 2024",
+      epoch: 430,
+      status: "Enacted",
+      title: "Plutus V3 Cost Model Parameter Changes Prior to Chang#2",
+      abstract:
+        "In light of recent network congestion and scalability concerns I propose implementing a dynamic fee adjustment mechanism that autonomously regulates transaction fees based on network demand and resource availability.",
+      governanceActionType: "Protocol Parameter Changes",
+      governanceActionID: "cnewjfbe82rg39udbjwbksomething",
+      cipGovernanceActionID: "cnewjfbe82rg39udbjwbksomething",
+      statusDate: "22nd Jan 2024",
+      statusEpoch: 440,
+    },
+  ];
+
   return (
     <Box>
-      <SearchFiltersSortBar
-        filterOptions={GOVERNANCE_ACTION_FILTERS}
-        statusOptions={GOVERNANCE_ACTION_STATUS_FILTERS}
-        sortOptions={GOVERNANCE_ACTION_SORT_OPTIONS}
-        searchText={searchText}
-        setSearchText={setSearchText}
-        selectedFilters={selectedFilters}
-        setSelectedFilters={setSelectedFilters}
-        closeFilters={closeFilters}
-        filtersOpen={filtersOpen}
-        setFiltersOpen={setFilterOpen}
-        closeSorts={closeSorts}
-        sortOpen={sortsOpen}
-        selectedSorting={selectedSorting}
-        setSelectedSorting={setSelectedSorting}
-        setSortOpen={setSortsOpen}
-      />
-      <Grid
+      <Box>
+        <SearchFiltersSortBar
+          filterOptions={GOVERNANCE_ACTION_FILTERS}
+          statusOptions={GOVERNANCE_ACTION_STATUS_FILTERS}
+          sortOptions={GOVERNANCE_ACTION_SORT_OPTIONS}
+          searchText={searchText}
+          setSearchText={setSearchText}
+          selectedFilters={selectedFilters}
+          setSelectedFilters={setSelectedFilters}
+          closeFilters={closeFilters}
+          filtersOpen={filtersOpen}
+          setFiltersOpen={setFilterOpen}
+          closeSorts={closeSorts}
+          sortOpen={sortsOpen}
+          selectedSorting={selectedSorting}
+          setSelectedSorting={setSelectedSorting}
+          setSortOpen={setSortsOpen}
+        />
+      </Box>
+      {/* <Grid
         container
         spacing={{ xs: 4, sm: 4, lg: 8 }}
         justifyContent={"center"}
@@ -85,7 +129,10 @@ export default function OutcomesPage() {
             statusEpoch={440}
           />
         </Grid>
-      </Grid>
+      </Grid> */}
+      <Box>
+        <GovernanceActionsList governanceActions={governanceActions} />
+      </Box>
     </Box>
   );
 }
