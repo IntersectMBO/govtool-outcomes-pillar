8969a851372893f47f49b01cb4ef0d78302eceee
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2025-05-28 12:14:36 +0300

    fix(Governance action navigation): support both cip-105 and cip-129 action identifiers for navigation

--- ui/src/hooks/useGetGovernanceActionQuery.ts
+++ ui/src/hooks/useGetGovernanceActionQuery.ts
@@ -2,12 +2,26 @@ import { useQuery } from "react-query";
 import { queryKeys } from "../consts/queryKeys";
 import { getGovernanceAction } from "../services/requests/getGovernanceAction";
 import { GovernanceAction } from "../types/api";
+import { decodeCIP129Identifier, getFullGovActionId } from "../lib/utils";
 
 export const useGetGovernanceActionQuery = (id: string) => {
+  const actionId = (() => {
+    if (id.startsWith("gov_action")) {
+      try {
+        const { txID } = decodeCIP129Identifier(id);
+        return getFullGovActionId(txID, 0);
+      } catch (error) {
+        console.log("Failed to decode gov_action identifier:", error);
+        return id;
+      }
+    }
+    return id;
+  })();
+  
   const { data, isLoading, error } = useQuery({
-    queryKey: [queryKeys.getGovernanceAction, id],
-    queryFn: async () => await getGovernanceAction(id),
-    enabled: !!id,
+    queryKey: [queryKeys.getGovernanceAction, actionId],
+    queryFn: async () => await getGovernanceAction(actionId),
+    enabled: !!actionId,
     refetchOnWindowFocus: false,
   });
 
