6285964bbfdbb67bce2da6810fca3e132c1a24e7
Kushal Acharya <kushal@sireto.io>
2026-07-28 13:50:57 +0545

    fix vulnerablities in backend

--- ui/src/hooks/useGetProposalQuery.ts
+++ ui/src/hooks/useGetProposalQuery.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 import { queryKeys } from "../consts/queryKeys";
 import { getProposal } from "../services/requests/getProposal";
 
